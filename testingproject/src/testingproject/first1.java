d0b1a0de1e8b36ec793b134fbbcd7087f4941e5d
ajaykanth19 <ajaykanth1921@gmail.com>
2020-08-19 15:18:18 +0530

    modified

--- testingproject/src/testingproject/first1.java
+++ testingproject/src/testingproject/first1.java
@@ -4,7 +4,7 @@ public class first1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=1;i<=10;i++)
+		for(int i=1;i<=5;i++)
 		{
 
 			System.out.println("Hello World..."+i);
